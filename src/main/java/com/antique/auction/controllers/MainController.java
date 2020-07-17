e3a17babd9637ae85617373b97dc0852a9108dee
lena <adamenkolena7@gmail.com>
2020-07-18 00:03:21 +0300

    add login page and authentication

--- src/main/java/com/antique/auction/controllers/MainController.java
+++ src/main/java/com/antique/auction/controllers/MainController.java
@@ -5,6 +5,7 @@ import com.antique.auction.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
@@ -16,12 +17,20 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String home () {
+    public String start () {
         Item item = new Item();
         item.setName("FirstLot");
         item.setDescription("this is first lot");
         item.setPrice(10);
         itemRepository.save(item);
         return "home";
+    }
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 }
