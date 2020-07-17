b907c7d17b663e99b33d68ca739503b8b84ff3c8
lena <adamenkolena7@gmail.com>
2020-07-17 17:29:42 +0300

    rename

--- src/main/java/com/antique/auction/AntiqueApplication.java
+++ src/main/java/com/antique/auction/AntiqueApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.antique.auction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AntiqueApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AntiqueApplication.class, args);
 	}
 
 }
